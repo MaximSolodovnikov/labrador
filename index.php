8f2f07a2f9d2a6cf2d794ae0ce10c3330bfd4065
MaximSolodovnikov <Gumolas@ukr.net>
2016-04-30 14:05:04 +0300

    Changed design

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             
             <div class="row" id="slider">
                 <div class="col-lg-12">
-                    <img class="img-responsive" src="images/goods/1.jpg" alt="">
+                    <img class="img-responsive" src="images/slider/1.jpg" alt="">
                 </div>
             </div>
             
